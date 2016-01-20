dbbeae7e2deb20e54327062a4b7456a22f1c263e
Werner Rau <werner@rau.be>
2016-01-20 14:27:39 +0100

    Added fade in en out

--- randomFileChooser.js
+++ randomFileChooser.js
@@ -1,9 +1,22 @@
 var files = [
-	"http://wernerrau.github.io/oef1.html",
-	"http://wernerrau.github.io/oef2.html"
+	"oef1.html",
+	"oef2.html"
 ]
 var numFiles = files.length;
 var chosenFileNr = Math.floor(Math.random()*numFiles);
 var chosenFile = files[chosenFileNr];
-var delay = 1000*60 // 1 minute
-setTimeout(function() {window.location = chosenFile;}, delay);
+var delay = 1000*20 // 1 minute
+function newLocation() {
+	window.location = chosenFile;
+}
+function fadeOut() {
+	$('body').fadeOut(2000,newLocation);
+}
+setTimeout(fadeOut,delay);
+$('body').css('display', 'none');
+jQuery(function($) {
+
+  $(document.body).fadeIn(5000);
+
+});
+
